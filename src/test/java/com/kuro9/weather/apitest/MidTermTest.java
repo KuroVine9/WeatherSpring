df9466e0cf61cc15a88368c6422e3ef88ee224b0
KuroVine9 <kurovine9@gmail.com>
2024-01-25 06:11:56 +0900

    docs response field 추가

--- src/test/java/com/kuro9/weather/apitest/MidTermTest.java
+++ src/test/java/com/kuro9/weather/apitest/MidTermTest.java
@@ -1,9 +1,12 @@
 package com.kuro9.weather.apitest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +22,38 @@ public class MidTermTest extends ApiTest {
                 .andDo(document("get-mid-term-forecast",
                         requestParameters(
                                 parameterWithName("regId").description("지역 코드(기상청 페이지 참조)")
-                        )));
+                        ),
+                        responseFields(
+                                fieldWithPath("regId").type(JsonFieldType.STRING).description("입력한 지역 코드"),
+                                fieldWithPath("tmFc").type(JsonFieldType.STRING).description("설정된 시간 코드(현재와 가장 가까운 코드로 자동 설정)"),
+                                fieldWithPath("rnSt3Am").type(JsonFieldType.NUMBER).description("3일 후 오전 강수 확률"),
+                                fieldWithPath("rnSt3Pm").type(JsonFieldType.NUMBER).description("3일 후 오후 강수 확률"),
+                                fieldWithPath("rnSt4Am").type(JsonFieldType.NUMBER).description("4일 후 오전 강수 확률"),
+                                fieldWithPath("rnSt4Pm").type(JsonFieldType.NUMBER).description("4일 후 오후 강수 확률"),
+                                fieldWithPath("rnSt5Am").type(JsonFieldType.NUMBER).description("5일 후 오전 강수 확률"),
+                                fieldWithPath("rnSt5Pm").type(JsonFieldType.NUMBER).description("5일 후 오후 강수 확률"),
+                                fieldWithPath("rnSt6Am").type(JsonFieldType.NUMBER).description("6일 후 오전 강수 확률"),
+                                fieldWithPath("rnSt6Pm").type(JsonFieldType.NUMBER).description("6일 후 오후 강수 확률"),
+                                fieldWithPath("rnSt7Am").type(JsonFieldType.NUMBER).description("7일 후 오전 강수 확률"),
+                                fieldWithPath("rnSt7Pm").type(JsonFieldType.NUMBER).description("7일 후 오후 강수 확률"),
+                                fieldWithPath("rnSt8").type(JsonFieldType.NUMBER).description("8일 후 강수 확률"),
+                                fieldWithPath("rnSt9").type(JsonFieldType.NUMBER).description("9일 후 강수 확률"),
+                                fieldWithPath("rnSt10").type(JsonFieldType.NUMBER).description("10일 후 강수 확률"),
+
+                                fieldWithPath("wf3Am").type(JsonFieldType.STRING).description("3일 후 오전 날씨 예보"),
+                                fieldWithPath("wf3Pm").type(JsonFieldType.STRING).description("3일 후 오후 날씨 예보"),
+                                fieldWithPath("wf4Am").type(JsonFieldType.STRING).description("4일 후 오전 날씨 예보"),
+                                fieldWithPath("wf4Pm").type(JsonFieldType.STRING).description("4일 후 오후 날씨 예보"),
+                                fieldWithPath("wf5Am").type(JsonFieldType.STRING).description("5일 후 오전 날씨 예보"),
+                                fieldWithPath("wf5Pm").type(JsonFieldType.STRING).description("5일 후 오후 날씨 예보"),
+                                fieldWithPath("wf6Am").type(JsonFieldType.STRING).description("6일 후 오전 날씨 예보"),
+                                fieldWithPath("wf6Pm").type(JsonFieldType.STRING).description("6일 후 오후 날씨 예보"),
+                                fieldWithPath("wf7Am").type(JsonFieldType.STRING).description("7일 후 오전 날씨 예보"),
+                                fieldWithPath("wf7Pm").type(JsonFieldType.STRING).description("7일 후 오후 날씨 예보"),
+                                fieldWithPath("wf8").type(JsonFieldType.STRING).description("8일 후 날씨 예보"),
+                                fieldWithPath("wf9").type(JsonFieldType.STRING).description("9일 후 날씨 예보"),
+                                fieldWithPath("wf10").type(JsonFieldType.STRING).description("10일 후 날씨 예보")
+                        )
+                ));
     }
 }
--- src/test/java/com/kuro9/weather/apitest/UltraTermTest.java
+++ src/test/java/com/kuro9/weather/apitest/UltraTermTest.java
@@ -1,9 +1,12 @@
 package com.kuro9.weather.apitest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -18,6 +21,19 @@ public class UltraTermTest extends ApiTest {
                         requestParameters(
                                 parameterWithName("nx").description("지역 좌표(기상청 페이지 참조)"),
                                 parameterWithName("ny").description("지역 좌표(기상청 페이지 참조)")
-                        )));
+                        ),
+                        responseFields(
+                                fieldWithPath("baseDate").type(JsonFieldType.STRING).description("설정된 날짜(현재와 가장 가까운 일자로 자동 설정)"),
+                                fieldWithPath("baseTime").type(JsonFieldType.STRING).description("설정된 시간(현재와 가장 가까운 시각으로 자동 설정)"),
+                                fieldWithPath("nx").type(JsonFieldType.NUMBER).description("입력한 좌표"),
+                                fieldWithPath("ny").type(JsonFieldType.NUMBER).description("입력한 좌표"),
+                                fieldWithPath("items").type(JsonFieldType.ARRAY).description("데이터 리스트"),
+                                fieldWithPath("items[].category").type(JsonFieldType.STRING).description("데이터 카테고리(기상청 페이지 참조)"),
+                                fieldWithPath("items[].fcstDate").type(JsonFieldType.STRING).description("예보의 해당 일자"),
+                                fieldWithPath("items[].fcstTime").type(JsonFieldType.STRING).description("예보의 해당 시각"),
+                                fieldWithPath("items[].fcstValue").type(JsonFieldType.STRING).description("예보 값")
+
+                        )
+                ));
     }
 }
